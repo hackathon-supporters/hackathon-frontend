ced67079b53114cebeb0581d28047e00c330abc5
10fu3 <msf.1192@gmail.com>
2022-06-04 15:26:48 +0900

    fix mypage design

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,18 +1,35 @@
-import {Box, Button, Center, chakra, Input, Wrap, WrapItem, Avatar, AvatarBadge, AvatarGroup} from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Center,
+    chakra,
+    Input,
+    Wrap,
+    WrapItem,
+    Avatar,
+    AvatarBadge,
+    AvatarGroup,
+    Flex
+} from "@chakra-ui/react";
 import {NextPage} from "next";
 
 const MyPage:NextPage = ()=>{
-    return <Center w={"full"} h="100vh">
-        <Center w={"600px"} h="100vh">
-        <Wrap>
-            <WrapItem>
+    return <Box display="flex" justifyContent="center" w={"full"} h="100vh">
+        <Box pt="100px">
+            <Flex justifyContent="space-between" h="128px" w={"600px"}>
                 <Avatar size='2xl' name='aaaa' src='https://pbs.twimg.com/media/EZu_kWdUEAUW6gq.jpg' />
-            </WrapItem>
-        </Wrap>
-        <chakra.h1>aaaa</chakra.h1>
-        <Button colorScheme='blue'>まずは話を聞いてみる</Button>
-        </Center>
-    </Center>;
+                <Center>
+                    <chakra.h1 fontSize="30px">aaaa</chakra.h1>
+                </Center>
+                <Center>
+                    <Button colorScheme='blue'>まずは話を聞いてみる</Button>
+                </Center>
+            </Flex>
+            <Box pt="100px">
+                aaa
+            </Box>
+        </Box>
+    </Box>;
 }
 
 export default MyPage
