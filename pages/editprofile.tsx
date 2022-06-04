a4ac415a5b78218f504ce47d59dfc8bc65221d35
10fu3 <msf.1192@gmail.com>
2022-06-04 17:02:20 +0900

    fix editprofile

--- pages/editprofile.tsx
+++ pages/editprofile.tsx
@@ -44,7 +44,9 @@ const CustomControlsExample:NextPage = ()=> {
         <Box display="flex" justifyContent="center" w={"full"} h="100vh">
             <Box pt="100px">
                 <Flex justifyContent="space-between" h="128px" w={"600px"}>
-                    <Avatar size='2xl' name='aaaa' src='https://pbs.twimg.com/media/EZu_kWdUEAUW6gq.jpg' />
+                    <Box>
+                        <Avatar size='2xl' name='aaaa' src='https://pbs.twimg.com/media/EZu_kWdUEAUW6gq.jpg' />
+                    </Box>
                     <Center>
                         <Editable
                             textAlign='center'
@@ -52,19 +54,25 @@ const CustomControlsExample:NextPage = ()=> {
                             isPreviewFocusable={false}
                             >
                             <EditablePreview />
-                            <Input as={EditableInput} />
-                            <EditableControls />
+                            <Flex>
+                                <Input as={EditableInput} />
+                                <EditableControls />
+                            </Flex>
                         </Editable>
                     </Center>
                     <Center>
                         <Button colorScheme='blue'>変更を保存</Button>
                     </Center>
                 </Flex>
-                <VStack divider={<StackDivider borderColor='gray.200' />} spacing={4} align='stretch'>
-                    <Box h='40px' bg='pink.100'>
+                <VStack pt={10} divider={<StackDivider borderColor='gray.200' />} spacing={4} align='stretch'>
+                    <Flex p={5} boxSizing="content-box" justifyContent="space-between" h='40px' bg='pink.100'>
                         <Avatar size='sm' name='company' src='https://pbs.twimg.com/profile_images/1161189849048600576/u_k9IPM2_400x400.jpg' />
                         <Editable
+                            pl={5}
+                            w="full"
+                            justifyContent="space-between"
                             textAlign='center'
+                            display="flex"
                             defaultValue='株式会社ウカツキコデマヌシ'
                             isPreviewFocusable={false}
                         >
@@ -72,7 +80,7 @@ const CustomControlsExample:NextPage = ()=> {
                             <Input as={EditableInput} />
                             <EditableControls />
                         </Editable>
-                    </Box>
+                    </Flex>
                 </VStack>
             </Box>
         </Box>
