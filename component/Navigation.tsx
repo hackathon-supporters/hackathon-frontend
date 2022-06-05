19950d5a0be6c93faee2784f9c8c806d791f07a7
H3KUN <bp20050@shibaura-it.ac.jp>
2022-06-05 13:13:36 +0900

    aaaaa

--- component/Navigation.tsx
+++ component/Navigation.tsx
@@ -1,7 +1,13 @@
 import {Box, Center, Flex, IconButton, Spacer} from "@chakra-ui/react";
-import {ChatIcon} from "@chakra-ui/icons";
+import {ChatIcon, EditIcon} from "@chakra-ui/icons";
+import {useRouter} from "next/router";
+import {loginStatusAtom} from "../atom/loginstatus";
 
 const Navigation = ()=>{
+    const router = useRouter(loginStatusAtom);
+    const VisitEdit=()=>{
+        router.push("/editprofile");
+    }
     return <Box borderBottom="1px solid #e9e9e9" w="full" h="64px">
         <Flex m={5}>
             <Center>
@@ -9,6 +15,10 @@ const Navigation = ()=>{
                     MatchQuiter
                 </Box>
             </Center>
+            <Spacer/>
+            <IconButton onClick={()=>{VisitEdit()}} aria-label={"edit"}>
+                <EditIcon/>
+            </IconButton>
             <Spacer/>
             <IconButton aria-label={"chat"}>
                 <ChatIcon/>
